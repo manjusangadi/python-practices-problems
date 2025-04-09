aaf0fba75182879bec8554b37d8d569ac62e9971
Manju Angadi <152906296+manjusangadi@users.noreply.github.com>
2025-04-09 22:09:39 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,7 @@
+
+#ask user name,location,college
+user_name=input('user_name=')
+user_location=input('userf-location=')
+useer_college_name=input('useer_college_name=')
+
+print(user_name + ' from '+ user_location +' is studing in ' + useer_college_name)
