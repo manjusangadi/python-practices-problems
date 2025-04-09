79528395f71a9501161e4452c15107e76295d52d
Manju Angadi <152906296+manjusangadi@users.noreply.github.com>
2025-04-09 22:42:44 +0530

    Update p2.py
    question change

--- p2.py
+++ p2.py
@@ -1,10 +1,3 @@
-# Write a Python program that:
-#flipcart discount for price
-# if google pay = 20% discount
-# else 10% discount
-# discount amount
-
-
 
 price = float(input('Enter the price of product :'))
 google_pay = True
