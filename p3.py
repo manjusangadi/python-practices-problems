eee89858d99ba6df6c081d811c1c8e482db3e407
Manju Angadi <152906296+manjusangadi@users.noreply.github.com>
2025-04-10 22:07:39 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,13 +1,4 @@
 # comparison operartor
-#Write a Python program that:
-# Asks the user to enter their name.
-# Checks the length of the name:
-# If the name has less than 3 characters, display:
-# "your name is simple "
-# If the name has more than 14 characters, display:
-# "your name is long"
-# Otherwise, display:
-# "your name is valid"
 
 user_name = input('Enter your name: ')
 
