e6144ffca7ce2670007685cbbb6322712f26b18f
Manju Angadi <152906296+manjusangadi@users.noreply.github.com>
2025-04-11 22:22:55 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,9 +1,9 @@
-# comparison operartor
+# comparison operartor 
 
 user_name = input('Enter your name: ')
 
 if len(user_name) <= 3:
-    print('your name is simple ')
+    print('your name is simple  ')
 elif len(user_name)>14:
     print('your name is long')
 else:
